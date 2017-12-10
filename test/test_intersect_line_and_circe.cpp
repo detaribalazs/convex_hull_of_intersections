edda717df89fa2680b919ed8f69543075c35425d
Gabor Koszegi <koszegi.gabor@gmail.com>
2017-12-10 16:21:38 +0100

    test: check intersection points are indeed on the lines and circles

--- test/test_intersect_line_and_circe.cpp
+++ test/test_intersect_line_and_circe.cpp
@@ -36,8 +36,11 @@ TEST_CASE(intersect_line_circle_origo_based_vertical_tangent)
     LineSegment l{Point{2,-10}, Point{2,10}};
     Circle c{Point{0,0}, 2};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{2,0}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{2,0}));
 }
 
 // =================================================================================================
@@ -46,9 +49,14 @@ TEST_CASE(intersect_line_circle_origo_based_vertical_full_crossing)
     LineSegment l{Point{0,-10}, Point{0,10}};
     Circle c{Point{0,0}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 2);
-    CHECK_EQ(intersect(l,c)[0], (Point{0, +3}));
-    CHECK_EQ(intersect(l,c)[1], (Point{0, -3}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
+    CHECK_EQ(isec[0], (Point{0, +3}));
+    CHECK_EQ(isec[1], (Point{0, -3}));
 }
 
 // =================================================================================================
@@ -57,8 +65,11 @@ TEST_CASE(intersect_line_circle_origo_based_vertical_upper_half_crossing)
     LineSegment l{Point{0,1}, Point{0,10}};
     Circle c{Point{0,0}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{0, +3}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{0, +3}));
 }
 
 // =================================================================================================
@@ -67,8 +78,11 @@ TEST_CASE(intersect_line_circle_origo_based_vertical_lower_half_crossing)
     LineSegment l{Point{0,1}, Point{0,-10}};
     Circle c{Point{0,0}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{0, -3}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{0, -3}));
 }
 
 // =================================================================================================
@@ -77,8 +91,11 @@ TEST_CASE(intersect_line_circle_vertical_tangent_translated)
     LineSegment l{Point{6,-10}, Point{6,10}};
     Circle c{Point{4,2}, 2};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{6,2}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{6,2}));
 }
 
 // =================================================================================================
@@ -87,9 +104,14 @@ TEST_CASE(intersect_line_circle_vertical_full_crossing_translated)
     LineSegment l{Point{3,-10}, Point{3,10}};
     Circle c{Point{3,1}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 2);
-    CHECK_EQ(intersect(l,c)[0], (Point{3, +4}));
-    CHECK_EQ(intersect(l,c)[1], (Point{3, -2}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
+    CHECK_EQ(isec[0], (Point{3, +4}));
+    CHECK_EQ(isec[1], (Point{3, -2}));
 }
 
 // =================================================================================================
@@ -98,8 +120,11 @@ TEST_CASE(intersect_line_circle_vertical_upper_half_crossing_translated)
     LineSegment l{Point{3,1}, Point{3,10}};
     Circle c{Point{3,1}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{3, +4}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{3, +4}));
 }
 
 // =================================================================================================
@@ -110,8 +135,13 @@ TEST_CASE(intersect_line_circle_origo_based_generic_tangent)
     LineSegment l{Point{0, sqrt2 * r}, Point{sqrt2*r, 0}};
     Circle c{Point{0,0}, r};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{sqrt2 * r / 2.0f, sqrt2 * r / 2.0f}));
+    auto ln = l.line();
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK_EQ(ln.a * isec[0].x + ln.b * isec[0].y, ln.c);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{sqrt2 * r / 2.0f, sqrt2 * r / 2.0f}));
 }
 
 // =================================================================================================
@@ -125,6 +155,10 @@ TEST_CASE(intersect_line_circle_origo_based_generic_crossing)
     CHECK_EQ(l.slope_y(), -1.0);
     auto isec = intersect(l,c);
     CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
     CHECK_EQ(std::max(isec[0], isec[1]), (Point{4.84814, 1.22293}));
     CHECK_EQ(std::min(isec[0], isec[1]), (Point{1.22293, 4.84814}));
 }
@@ -135,8 +169,11 @@ TEST_CASE(intersect_line_circle_origo_based_generic_upper_half_crossing)
     LineSegment l{Point{0, 7}, Point{3, 3}};
     Circle c{Point{0,0}, 5};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{1.73224, 4.69035}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{1.73224, 4.69035}));
 }
 
 // =================================================================================================
@@ -150,6 +187,10 @@ TEST_CASE(intersect_line_circle_origo_based_generic_crossing_backward_line)
     CHECK_EQ(l.slope_y(), -1.0);
     auto isec = intersect(l,c);
     CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
     CHECK_EQ(std::max(isec[0], isec[1]), (Point{4.84814, 1.22293}));
     CHECK_EQ(std::min(isec[0], isec[1]), (Point{1.22293, 4.84814}));
 }
@@ -162,8 +203,11 @@ TEST_CASE(intersect_line_circle_origo_based_upper_crossing_positive_slope)
     const float srr = sqrt(c.radius * c.radius / 2.0);
 
     CHECK_EQ(l.slope_y(), 1.0);
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{srr, srr}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{srr, srr}));
 }
 
 // =================================================================================================
@@ -174,8 +218,11 @@ TEST_CASE(intersect_line_circle_origo_based_lower_crossing_positive_slope)
     const float srr = sqrt(c.radius * c.radius / 2.0);
 
     CHECK_EQ(l.slope_y(), 1.0);
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{-srr, -srr}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{-srr, -srr}));
 }
 
 // =================================================================================================
@@ -186,9 +233,14 @@ TEST_CASE(intersect_line_circle_origo_based_full_crossing_positive_slope)
     const float srr = sqrt(c.radius * c.radius / 2.0);
 
     CHECK_EQ(l.slope_y(), 1.0);
-    CHECK_EQ(intersect(l,c).size(), 2);
-    CHECK_EQ(intersect(l,c)[0], (Point{srr, srr}));
-    CHECK_EQ(intersect(l,c)[1], (Point{-srr, -srr}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
+    CHECK_EQ(isec[0], (Point{srr, srr}));
+    CHECK_EQ(isec[1], (Point{-srr, -srr}));
 }
 
 // =================================================================================================
@@ -197,8 +249,11 @@ TEST_CASE(intersect_line_circle_origo_based_horizontal_tangent)
     LineSegment l{Point{-10, 2}, Point{10, 2}};
     Circle c{Point{0,0}, 2};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{0,2}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK_EQ(isec[0], (Point{0,2}));
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
 }
 
 // =================================================================================================
@@ -207,9 +262,14 @@ TEST_CASE(intersect_line_circle_origo_based_horizontal_full_crossing)
     LineSegment l{Point{-10, 0}, Point{10, 0}};
     Circle c{Point{0,0}, 3};
 
-    CHECK_EQ(intersect(l,c).size(), 2);
-    CHECK_EQ(intersect(l,c)[0], (Point{-3, 0}));
-    CHECK_EQ(intersect(l,c)[1], (Point{+3, 0}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
+    CHECK_EQ(isec[0], (Point{-3, 0}));
+    CHECK_EQ(isec[1], (Point{+3, 0}));
 }
 
 // =================================================================================================
@@ -218,9 +278,14 @@ TEST_CASE(intersect_line_circle_horizontal_full_crossing_translated)
     LineSegment l{Point{-10, 1}, Point{10, 1}};
     Circle c{Point{3,1}, 2};
 
-    CHECK_EQ(intersect(l,c).size(), 2);
-    CHECK_EQ(intersect(l,c)[0], (Point{1, 1}));
-    CHECK_EQ(intersect(l,c)[1], (Point{5, 1}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 2);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(l.line().valid(isec[1]));
+    CHECK(c.valid(isec[0]));
+    CHECK(c.valid(isec[1]));
+    CHECK_EQ(isec[0], (Point{1, 1}));
+    CHECK_EQ(isec[1], (Point{5, 1}));
 }
 
 // =================================================================================================
@@ -229,6 +294,9 @@ TEST_CASE(intersect_line_circle_horizontal_tangent_translated)
     LineSegment l{Point{-10, 3}, Point{10, 3}};
     Circle c{Point{3,1}, 2};
 
-    CHECK_EQ(intersect(l,c).size(), 1);
-    CHECK_EQ(intersect(l,c)[0], (Point{3, 3}));
+    auto isec = intersect(l,c);
+    CHECK_EQ(isec.size(), 1);
+    CHECK(l.line().valid(isec[0]));
+    CHECK(c.valid(isec[0]));
+    CHECK_EQ(isec[0], (Point{3, 3}));
 }
