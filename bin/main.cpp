09b90818f66706aaff55f230ad3adf907fd140a0
Gabor Koszegi <koszegi.gabor@gmail.com>
2017-12-11 21:10:16 +0100

    cleanup: formatting

--- bin/main.cpp
+++ bin/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <iterator>
 
+// =================================================================================================
 int main()
 {
     unsigned numShapes;
--- lib/OutputWriter.cpp
+++ lib/OutputWriter.cpp
@@ -4,7 +4,7 @@
 
 using namespace gk;
 
-
+// =================================================================================================
 OutputPointsIterator& OutputPointsIterator::operator= (const Point& point)
 {
     out << std::fixed << std::setprecision(4) << point.x << ' ' << point.y << '\n';
